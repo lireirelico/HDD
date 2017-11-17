dc550cff60958d0b8ecfc946540ba0c9918a67f0
Eugene Pantelei (#550501) <eugene.pantelei@gmail.com>
2017-11-17 19:14:04 +0300

    Add files via upload

---
+++ IiPUlab2/IiPUlab2/IiPUlab2/Source.cpp
@@ -0,0 +1,191 @@
+#include <iostream>
+#include <iomanip>
+#include <Windows.h>
+#include <WinIoCtl.h>
+#include <ntddscsi.h>
+#include <conio.h>
+
+using namespace std;
+
+#define bThousand 1024
+#define Hundred 100
+#define BYTE_SIZE 8
+
+char* busType[] = { "UNKNOWN", "SCSI", "ATAPI", "ATA", "ONE_TREE_NINE_FOUR", "SSA", "FIBRE", "USB", "RAID", "ISCSI", "SAS", "SATA", "SD", "MMC" };
+
+void getDeviceInfo(HANDLE diskHandle, STORAGE_PROPERTY_QUERY storagePropertyQuery) {
+
+	//Выделение памяти под дескриптор устройства
+	STORAGE_DEVICE_DESCRIPTOR* deviceDescriptor = (STORAGE_DEVICE_DESCRIPTOR*)calloc(bThousand, 1);
+	DWORD dwBytesReturned = 0;
+
+	//Отправляем управляющий код непосредственно указанному драйверу устройства, заставляя устройство выполнить соответствующую операцию.
+	if (!DeviceIoControl(diskHandle,
+		IOCTL_STORAGE_QUERY_PROPERTY,				//Отправляем запрос на возврат свойств устройства.
+		&storagePropertyQuery, sizeof(storagePropertyQuery), deviceDescriptor, bThousand, &dwBytesReturned, 0)) {
+		printf("%d", GetLastError());
+		CloseHandle(diskHandle);
+		exit(-1);
+	}
+
+	//Выводим свойства диска.
+	cout << "Model:    " << setw(30) << (char*)(deviceDescriptor)+deviceDescriptor->ProductIdOffset << endl;
+	cout << "Version        " << (char*)(deviceDescriptor)+deviceDescriptor->ProductRevisionOffset << endl;
+	cout << "Bus type:      " << busType[deviceDescriptor->BusType] << endl;
+	cout << "Serial number: " << (char*)(deviceDescriptor)+deviceDescriptor->SerialNumberOffset << endl;
+}
+
+void getMemoryInfo() {
+	string path;
+	_ULARGE_INTEGER diskSpace;
+	_ULARGE_INTEGER freeSpace;
+
+	diskSpace.QuadPart = 0;
+	freeSpace.QuadPart = 0;
+
+	_ULARGE_INTEGER totalDiskSpace;
+	_ULARGE_INTEGER totalFreeSpace;
+
+	totalDiskSpace.QuadPart = 0;				//Будет содержать полный размер диска.
+	totalFreeSpace.QuadPart = 0;				//Будет содеражть свободное место диска.
+
+	//Получаем битовую маску, представляющую имеющиеся в настоящие время дисковые накопители.
+	unsigned long int logicalDrivesCount = GetLogicalDrives();
+
+	cout.setf(ios::left);
+	cout << setw(16) << "Total space[Mb]"
+		<< setw(16) << "Free space[Mb]"
+		<< setw(16) << "Busy space[%]"
+		<< endl;
+
+	//Анализ полученной битовой маски(бит 0 - диск А, бит 1 - диск B).
+	for (char var = 'C'; var <= 67; var++) {
+		if ((logicalDrivesCount >> var - 65) & 1 && var != 'F') {
+			path = var;
+			path.append(":\\");
+			//Получаем информация о размере диска и свободном пространстве диска.
+			GetDiskFreeSpaceEx(path.c_str(), 0, &diskSpace, &freeSpace);
+			diskSpace.QuadPart = diskSpace.QuadPart / (bThousand * bThousand);
+			freeSpace.QuadPart = freeSpace.QuadPart / (bThousand * bThousand);
+
+			//Определяем тип диска(3 - жесткий диск)
+			if (GetDriveType(path.c_str()) == 3) {
+				totalDiskSpace.QuadPart += diskSpace.QuadPart;
+				totalFreeSpace.QuadPart += freeSpace.QuadPart;
+			}
+		}
+	}
+
+	cout << setw(16) << totalDiskSpace.QuadPart
+		<< setw(16) << totalFreeSpace.QuadPart
+		<< setw(16) << setprecision(3) << 100.0 - (double)totalFreeSpace.QuadPart / (double)totalDiskSpace.QuadPart * Hundred
+		<< endl;
+}
+
+void getAtaPioDmaSupportStandarts(HANDLE diskHandle) {
+
+	UCHAR identifyDataBuffer[512 + sizeof(ATA_PASS_THROUGH_EX)] = { 0 };
+
+	ATA_PASS_THROUGH_EX &PTE = *(ATA_PASS_THROUGH_EX *)identifyDataBuffer;	//Структура для отправки АТА команды устройству
+	PTE.Length = sizeof(PTE);
+	PTE.TimeOutValue = 10;									//Размер структуры
+	PTE.DataTransferLength = 512;							//Размер буфера для данных
+	PTE.DataBufferOffset = sizeof(ATA_PASS_THROUGH_EX);		//Смещение в байтах от начала структуры до буфера данных
+	PTE.AtaFlags = ATA_FLAGS_DATA_IN;						//Флаг, говорящий о чтении байтов из устройства
+
+	IDEREGS *ideRegs = (IDEREGS *)PTE.CurrentTaskFile;
+	ideRegs->bCommandReg = 0xEC;
+
+	DWORD dwBytesReturned = 0;
+
+	//Производим запрос устройству
+	if (!DeviceIoControl(diskHandle,
+		IOCTL_ATA_PASS_THROUGH,								//Флаг, говорящий что мы посылаем структуру с командами типа ATA_PASS_THROUGH_EX
+		&PTE, sizeof(identifyDataBuffer), &PTE, sizeof(identifyDataBuffer), &dwBytesReturned, NULL)) {
+		cout << GetLastError() << std::endl;
+		return;
+	}
+	WORD *data = (WORD *)(identifyDataBuffer + sizeof(ATA_PASS_THROUGH_EX));	//Получаем указатель на массив полученных данных
+	short ataSupportByte = data[80];
+	int i = 2 * BYTE_SIZE;
+	int bitArray[2 * BYTE_SIZE];
+	//Превращаем байты с информацией о поддержке ATA в массив бит
+	while (i--) {
+		bitArray[i] = ataSupportByte & 32768 ? 1 : 0;
+		ataSupportByte = ataSupportByte << 1;
+	}
+
+	//Анализируем полученный массив бит.
+	cout << "ATA Support:   ";
+	for (int i = 8; i >= 4; i--) {
+		if (bitArray[i] == 1) {
+			cout << "ATA" << i;
+			if (i != 4) {
+				cout << ", ";
+			}
+		}
+	}
+	cout << endl;
+
+	//Вывод поддерживаемых режимов DMA
+	unsigned short dmaSupportedBytes = data[63];
+	int i2 = 2 * BYTE_SIZE;
+	//Превращаем байты с информацией о поддержке DMA в массив бит
+	while (i2--) {
+		bitArray[i2] = dmaSupportedBytes & 32768 ? 1 : 0;
+		dmaSupportedBytes = dmaSupportedBytes << 1;
+	}
+
+	//Анализируем полученный массив бит.
+	cout << "DMA Support:   ";
+	for (int i = 0; i <8; i++) {
+		if (bitArray[i] == 1) {
+			cout << "DMA" << i;
+			if (i != 2) cout << ", ";
+		}
+	}
+	cout << endl;
+
+	unsigned short pioSupportedBytes = data[63];
+	int i3 = 2 * BYTE_SIZE;
+	//Превращаем байты с информацией о поддержке PIO в массив бит
+	while (i3--) {
+		bitArray[i3] = pioSupportedBytes & 32768 ? 1 : 0;
+		pioSupportedBytes = pioSupportedBytes << 1;
+	}
+
+	//Анализируем полученный массив бит.
+	cout << "PIO Support:   ";
+	for (int i = 0; i <2; i++) {
+		if (bitArray[i] == 1) {
+			cout << "PIO" << i + 3;
+			if (i != 1) cout << ", ";
+		}
+	}
+	cout << endl;
+}
+
+void init(HANDLE& diskHandle) {
+	//Открытие файла с информацией о диске
+	diskHandle = CreateFile("\\\\.\\PhysicalDrive0", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_EXISTING, NULL, NULL);
+	if (diskHandle == INVALID_HANDLE_VALUE) {
+		cout << GetLastError();
+		_getch();
+		return;
+	}
+}
+
+int main()
+{
+	STORAGE_PROPERTY_QUERY storagePropertyQuery;				//Структура с информацией об запросе
+	storagePropertyQuery.QueryType = PropertyStandardQuery;		//Запрос драйвера, чтобы он вернул дескриптор устройства.
+	storagePropertyQuery.PropertyId = StorageDeviceProperty;	//Флаг, гооврящий мы хотим получить дескриптор устройства.
+	HANDLE diskHandle;
+
+	init(diskHandle);
+	getDeviceInfo(diskHandle, storagePropertyQuery);
+	getMemoryInfo();
+	getAtaPioDmaSupportStandarts(diskHandle);
+	_getch();
+	return 0;
+}
